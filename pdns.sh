d0079128625c5ebec927c6620b082f6422ff787d
Tobias <faxmail09@gmx.net>
2016-09-09 12:17:25 +0200

    added several quoting fixes

--- pdns.sh
+++ pdns.sh
@@ -10,10 +10,10 @@ set -u
 set -o pipefail
 umask 077
 
-sql_base="pdns"
-sql_host="localhost"
-sql_user="root"
-sql_pass="password"
+mysql_base="pdns"
+mysql_host="localhost"
+mysql_user="root"
+mysql_pass="secretpassword"
 
 domain="${2}"
 token="${4}"
@@ -25,16 +25,16 @@ root_domain="${myarray_domain[*]: -2:1}.${myarray_domain[*]: -1:1}"
 done="no"
 
 if [[ "$1" = "deploy_challenge" ]]; then
-   id=`mysql -h'$mysql_host' -u'$mysql_user' -p'$mysql_pass' -s -N -e "SELECT id FROM '$mysql_base'.domains WHERE name='$root_domain'"`
-   soa=`mysql -h'$mysql_host' -u'$mysql_user' -p'$mysql_pass' -s -N -e "SELECT content FROM '$mysql_base'.records WHERE domain_id='$id' AND type='SOA'"`
-   idSoa=`mysql -h'$mysql_host' -u'$mysql_user' -p'$mysql_pass' -s -N -e "SELECT id FROM '$mysql_base'.records WHERE domain_id='$id' AND type='SOA'"`
+   id=`mysql -h"$mysql_host" -u"$mysql_user" -p"$mysql_pass" -s -N -e "SELECT id FROM $mysql_base.domains WHERE name='$root_domain';"`
+   soa=`mysql -h"$mysql_host" -u"$mysql_user" -p"$mysql_pass" -s -N -e "SELECT content FROM $mysql_base.records WHERE domain_id='$id' AND type='SOA'"`
+   idSoa=`mysql -h"$mysql_host" -u"$mysql_user" -p"$mysql_pass" -s -N -e "SELECT id FROM $mysql_base.records WHERE domain_id='$id' AND type='SOA'"`
    IFS=' ' read -r -a soArray <<< "$soa"
    soArray[2]=$((soArray[2]+1))
    soaNew=$( IFS=$' '; echo "${soArray[*]}" )
-   mysql -h'$mysql_host' -u'$mysql_user' -p'$mysql_pass' -s -e "UPDATE '$mysql_base'.records SET content='$soaNew' WHERE id='$idSoa'"
-   mysql -h'$mysql_host' -u'$mysql_user' -p'$mysql_pass' -s -e "INSERT INTO '$mysql_base'.records (id,domain_id,name,type,content,ttl,prio,change_date) VALUES ('', '$id', '_acme-challenge.$domain','TXT','\"$token\"','5','0','$timestamp')"
+   mysql -h"$mysql_host" -u"$mysql_user" -p"$mysql_pass" -s -e "UPDATE $mysql_base.records SET content='$soaNew' WHERE id='$idSoa'"
+   mysql -h"$mysql_host" -u"$mysql_user" -p"$mysql_pass" -s -e "INSERT INTO $mysql_base.records (id,domain_id,name,type,content,ttl,prio,change_date) VALUES ('', '$id', '_acme-challenge.$domain','TXT','$token','5','0','$timestamp')"
 
-  while ! dig +trace +short -t TXT _acme-challenge.$domain | grep -- "$token" > /dev/null
+  while ! dig +short -t TXT _acme-challenge.$domain | grep -- "$token" > /dev/null
     do
        printf "."
        sleep 3
@@ -43,7 +43,7 @@ if [[ "$1" = "deploy_challenge" ]]; then
 fi
 
 if [[ "$1" = "clean_challenge" ]]; then
-    mysql -h'$mysql_host' -u'$mysql_user' -p'$mysql_pass' -s -e "DELETE FROM '$mysql_base'.records WHERE content = '\"$token\"' AND type = 'TXT'"
+    mysql -h"$mysql_host" -u"$mysql_user" -p"$mysql_pass" -s -e "DELETE FROM $mysql_base.records WHERE content = '$token' AND type = 'TXT'"
     done="yes"
 fi
 
